eafb7c55bd0c328c3a6647e1568665d8c0e4dc92
AwesomeZachYT <zach.the.awesome.one@gmail.com>
2024-01-03 13:06:46 -0800

    Header Formatting

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-var header = document.getElementsByClassName("header-outside")[0];
+var header = document.getElementsByClassName("header")[0];
 var main_logo = document.getElementsByClassName("main-logo")[0];
 
 document.addEventListener("scroll", function() {
-  if (window.scrollY > 150) {
+  if (window.scrollY > 50) {
     header.style.position = "fixed";
-    main_logo.style.top = "110px";
-    main_logo.style.height = "80px";
   }
   else {
-    header.style.position = "relative";
-    main_logo.style.top = "10px";
-    main_logo.style.height = "180px";
+    header.style.position = "absolute";
   }
 });
